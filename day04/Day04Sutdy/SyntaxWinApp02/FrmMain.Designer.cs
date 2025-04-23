ba05b62dd19fedc057c7db09cdeea7487903bf97
phm0423 <qkrgusalsok@gmail.com>
2025-04-23 17:15:22 +0900

    4일차 커밋

---
+++ day04/Day04Sutdy/SyntaxWinApp02/FrmMain.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace SyntaxWinApp02
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            BtnCheck = new Button();
+            imageList1 = new ImageList(components);
+            label1 = new Label();
+            CboArray = new ComboBox();
+            label2 = new Label();
+            CboList = new ComboBox();
+            label3 = new Label();
+            CboDictionary = new ComboBox();
+            SuspendLayout();
+            // 
+            // BtnCheck
+            // 
+            BtnCheck.ImageAlign = ContentAlignment.MiddleLeft;
+            BtnCheck.ImageIndex = 0;
+            BtnCheck.ImageList = imageList1;
+            BtnCheck.Location = new Point(472, 259);
+            BtnCheck.Name = "BtnCheck";
+            BtnCheck.Padding = new Padding(10, 0, 0, 0);
+            BtnCheck.Size = new Size(100, 40);
+            BtnCheck.TabIndex = 0;
+            BtnCheck.Text = "데이터";
+            BtnCheck.UseVisualStyleBackColor = true;
+            BtnCheck.Click += BtnCheck_Click;
+            // 
+            // imageList1
+            // 
+            imageList1.ColorDepth = ColorDepth.Depth32Bit;
+            imageList1.ImageStream = (ImageListStreamer)resources.GetObject("imageList1.ImageStream");
+            imageList1.TransparentColor = Color.Transparent;
+            imageList1.Images.SetKeyName(0, "cs0006.png");
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(41, 20);
+            label1.Name = "label1";
+            label1.Size = new Size(78, 15);
+            label1.TabIndex = 1;
+            label1.Text = "배열로 할당: ";
+            // 
+            // CboArray
+            // 
+            CboArray.FormattingEnabled = true;
+            CboArray.Location = new Point(125, 12);
+            CboArray.Name = "CboArray";
+            CboArray.Size = new Size(121, 23);
+            CboArray.TabIndex = 2;
+            CboArray.SelectedIndexChanged += CboArray_SelectedIndexChanged;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(29, 49);
+            label2.Name = "label2";
+            label2.Size = new Size(90, 15);
+            label2.TabIndex = 1;
+            label2.Text = "리스트로 할당: ";
+            // 
+            // CboList
+            // 
+            CboList.FormattingEnabled = true;
+            CboList.Location = new Point(125, 41);
+            CboList.Name = "CboList";
+            CboList.Size = new Size(121, 23);
+            CboList.TabIndex = 2;
+            CboList.SelectedIndexChanged += CboList_SelectedIndexChanged;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(17, 73);
+            label3.Name = "label3";
+            label3.Size = new Size(102, 15);
+            label3.TabIndex = 1;
+            label3.Text = "딕셔너리로 할당: ";
+            // 
+            // CboDictionary
+            // 
+            CboDictionary.FormattingEnabled = true;
+            CboDictionary.Location = new Point(125, 70);
+            CboDictionary.Name = "CboDictionary";
+            CboDictionary.Size = new Size(121, 23);
+            CboDictionary.TabIndex = 2;
+            CboDictionary.SelectedIndexChanged += CboDictionary_SelectedIndexChanged;
+            // 
+            // FrmMain
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(584, 311);
+            Controls.Add(CboDictionary);
+            Controls.Add(CboList);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(CboArray);
+            Controls.Add(label1);
+            Controls.Add(BtnCheck);
+            Name = "FrmMain";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "컬렉션학습 윈앱";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button BtnCheck;
+        private Label label1;
+        private ComboBox CboArray;
+        private ImageList imageList1;
+        private Label label2;
+        private ComboBox CboList;
+        private Label label3;
+        private ComboBox CboDictionary;
+    }
+}
---
+++ day04/Day04Sutdy/SyntaxWinApp03/FrmMain.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace SyntaxWinApp03
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            BtnSave = new Button();
+            BtnLoad = new Button();
+            RtbNote = new RichTextBox();
+            BtnBold = new Button();
+            BtnRed = new Button();
+            DlgOpen = new OpenFileDialog();
+            DlgSave = new SaveFileDialog();
+            DlgColor = new ColorDialog();
+            SuspendLayout();
+            // 
+            // BtnSave
+            // 
+            BtnSave.Location = new Point(366, 269);
+            BtnSave.Name = "BtnSave";
+            BtnSave.Size = new Size(100, 30);
+            BtnSave.TabIndex = 0;
+            BtnSave.Text = "저장";
+            BtnSave.UseVisualStyleBackColor = true;
+            BtnSave.Click += BtnSave_Click;
+            // 
+            // BtnLoad
+            // 
+            BtnLoad.Location = new Point(472, 269);
+            BtnLoad.Name = "BtnLoad";
+            BtnLoad.Size = new Size(100, 30);
+            BtnLoad.TabIndex = 0;
+            BtnLoad.Text = "읽기";
+            BtnLoad.UseVisualStyleBackColor = true;
+            BtnLoad.Click += BtnLoad_Click;
+            // 
+            // RtbNote
+            // 
+            RtbNote.Font = new Font("나눔고딕", 8.999999F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            RtbNote.Location = new Point(12, 12);
+            RtbNote.Name = "RtbNote";
+            RtbNote.ScrollBars = RichTextBoxScrollBars.ForcedBoth;
+            RtbNote.Size = new Size(560, 242);
+            RtbNote.TabIndex = 1;
+            RtbNote.Text = "";
+            RtbNote.WordWrap = false;
+            // 
+            // BtnBold
+            // 
+            BtnBold.Location = new Point(330, 269);
+            BtnBold.Name = "BtnBold";
+            BtnBold.Size = new Size(30, 30);
+            BtnBold.TabIndex = 2;
+            BtnBold.Text = "B";
+            BtnBold.UseVisualStyleBackColor = true;
+            BtnBold.Click += BtnBold_Click;
+            // 
+            // BtnRed
+            // 
+            BtnRed.ForeColor = Color.Red;
+            BtnRed.Location = new Point(294, 269);
+            BtnRed.Name = "BtnRed";
+            BtnRed.Size = new Size(30, 30);
+            BtnRed.TabIndex = 2;
+            BtnRed.Text = "R";
+            BtnRed.UseVisualStyleBackColor = true;
+            BtnRed.Click += BtnRed_Click;
+            // 
+            // DlgOpen
+            // 
+            DlgOpen.FileName = "openFileDialog1";
+            // 
+            // FrmMain
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(584, 311);
+            Controls.Add(BtnRed);
+            Controls.Add(BtnBold);
+            Controls.Add(RtbNote);
+            Controls.Add(BtnLoad);
+            Controls.Add(BtnSave);
+            Name = "FrmMain";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "파일IO";
+            Load += Form1_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button BtnSave;
+        private Button BtnLoad;
+        private RichTextBox RtbNote;
+        private Button BtnBold;
+        private Button BtnRed;
+        private OpenFileDialog DlgOpen;
+        private SaveFileDialog DlgSave;
+        private ColorDialog DlgColor;
+    }
+}
